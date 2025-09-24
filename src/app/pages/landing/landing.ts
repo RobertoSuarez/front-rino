e6ac6773874d930eb2f8d615da53d82ced2ffef3
Rino Arias <rinoariaslaaz@gmail.com>
2025-09-24 01:00:20 -0500

    cambios en landing page

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -19,7 +19,7 @@ import { TestimonialsWidget } from './components/testimonialswidget';
     template: `
         <div class="bg-surface-0 dark:bg-surface-900">
             <div class="mx-auto" style="max-width: 1440px;">
-                <topbar-widget class="py-6 px-6 flex items-center justify-between relative lg:static" />
+                <topbar-widget class="py-4 sm:py-6 px-4 sm:px-6 flex items-center justify-between relative lg:static" />
             </div>
             <div id="home" class="landing-wrapper overflow-hidden">
                 <div class="mx-auto" style="max-width: 1440px;">
